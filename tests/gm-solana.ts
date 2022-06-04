c389cbadbe5c835d15a68d8c09b884d33365502b
Julien <juliencaron@protonmail.com>
2022-06-04 16:58:33 +0200

    test: add unit test for gm initialisation

--- tests/gm-solana.ts
+++ tests/gm-solana.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { GmSolana } from "../target/types/gm_solana";
+import assert from "assert";
+
+// we need to access SystemProgram so that we can create the base
+const { SystemProgram } = anchor.web3;
 
 describe("gm-solana", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.GmSolana as Program<GmSolana>;
+  let _baseAccount: anchor.web3.Keypair;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("creates a base account for gm's", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    // call the initialize function via RPC
+    const tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount]
+    });
+
+    // fetch the base account
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+    // gmCount is a "BigNumber" type, so we need to convert it to a string
+    assert.equal(account.gmCount.toString(), "0");
+
+    _baseAccount = baseAccount;
   });
 });
