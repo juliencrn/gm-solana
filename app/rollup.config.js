3e12061bfe1d49411c9a545d5732e1cda16c402d
Julien <juliencaron@protonmail.com>
2022-06-05 17:24:43 +0200

    dapp: connect wallet

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -6,6 +6,9 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import json from "@rollup/plugin-json";
+import builtins from "rollup-plugin-node-builtins";
+import globals from "rollup-plugin-node-globals";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -59,6 +62,11 @@ export default {
 			browser: true,
 			dedupe: ['svelte']
 		}),
+		resolve({
+			browser: true,
+			dedupe: ["svelte"],
+			preferBuiltins: false, // set this to false
+		}),
 		commonjs(),
 		typescript({
 			sourceMap: !production,
@@ -75,7 +83,11 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+		json(),
+		globals(),
+		builtins(),
 	],
 	watch: {
 		clearScreen: false
--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,81 @@
 <script lang="ts">
-	export let name: string;
+  import { onMount } from "svelte"
+
+  export let name: string
+
+  // ======== APPLICATION STATE ========
+
+  let wallet: any
+  let account = ""
+
+  // reactively log the wallet connection when account state changes,
+  $: account && console.log(`Connected to wallet: ${account}`)
+
+  // ======== PAGE LOAD CHECKS ========
+
+  const onLoad = async () => {
+    const { solana } = window as any
+    wallet = solana
+
+    // set up handlers for wallet events
+    wallet.on("connect", () => (account = wallet.publicKey.toString()))
+    wallet.on("disconnect", () => (account = ""))
+
+    // eagerly connect wallet if the user already has connected before, otherwise do nothing
+    const resp = await wallet.connect({ onlyIfTrusted: true })
+  }
+
+  // life cycle hook for when the component is mounted
+  onMount(() => {
+    // run the onLoad function when the page completes loading
+    window.addEventListener("load", onLoad)
+
+    // return a cleanup function to remove the event listener to avoid memory leaks when the page unloads
+    return () => window.removeEventListener("load", onLoad)
+  })
+
+  // ======== CONNECT WALLET ========
+  const handleConnectWallet = async () => {
+    const resp = await wallet.connect()
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>gm, {name}!</h1>
+
+  <!-- Conditionally render the user account, connect button, or just a warning -->
+  {#if account}
+    <h3>Your wallet:</h3>
+    <p>{account}</p>
+  {:else if wallet}
+    {#if wallet.isPhantom}
+      <h2>Phantom Wallet found!</h2>
+      <button on:click={handleConnectWallet}>Connect wallet</button>
+    {:else}
+      <h2>Solana wallet found but not supported.</h2>
+    {/if}
+  {:else}
+    <h2>Solana wallet not found.</h2>
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
